9b6d1aa382110e57afa5388531f3f2094cc0d488
hiendbui <hiendbui0@gmail.com>
2020-12-07 15:25:09 -0800

    Write session api util ajax requests

--- frontend/optIn.jsx
+++ frontend/optIn.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import * as SessionAPIUtil from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
     const rootEl = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to OptIn</h1>, rootEl);
 });
+
+// window.signup = SessionAPIUtil.signup;
+// window.login = SessionAPIUtil.login;
+// window.logout = SessionAPIUtil.logout;
