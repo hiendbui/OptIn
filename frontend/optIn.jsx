f734578353fc26eb866f85d1e1c91ccb8184894c
hiendbui <hiendbui0@gmail.com>
2020-12-07 15:46:52 -0800

    Add configureStore and test successfully

--- frontend/optIn.jsx
+++ frontend/optIn.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import * as SessionAPIUtil from "./util/session_api_util";
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
     const rootEl = document.getElementById("root");
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     ReactDOM.render(<h1>Welcome to OptIn</h1>, rootEl);
 });
-
-// window.signup = SessionAPIUtil.signup;
-// window.login = SessionAPIUtil.login;
-// window.logout = SessionAPIUtil.logout;
