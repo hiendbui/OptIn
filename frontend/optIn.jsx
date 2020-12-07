9a1953b012cbffa237f461be7287dc400dd33f3f
hiendbui <hiendbui0@gmail.com>
2020-12-07 15:56:23 -0800

    Create root and app components

--- frontend/optIn.jsx
+++ frontend/optIn.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const rootEl = document.getElementById("root");
     const store = configureStore();
     window.getState = store.getState;
     window.dispatch = store.dispatch;
-    ReactDOM.render(<h1>Welcome to OptIn</h1>, rootEl);
+    ReactDOM.render(<Root store={store}/>, rootEl);
 });
