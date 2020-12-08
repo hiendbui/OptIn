0d9726b3a408ed659669666642be18934b4b0e5b
hiendbui <hiendbui0@gmail.com>
2020-12-07 18:31:44 -0800

    Add session form containers

---
+++ frontend/components/session_form/login_form_container.jsx
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { login } from '../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mapStateToProps = state => ({
+    errors: state.errors.session,
+    formType: 'login'
+})
+
+const mapDispatchToProps = dispatch => ({
+    processForm: (user) => login(user)
+})
+const LoginFormContainer = connect(mapStateToProps,mapDispatchToProps)(SessionForm);
+export default LoginFormContainer;
---
+++ frontend/components/session_form/session_form.jsx
@@ -0,0 +1,11 @@
+class SessionForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        };
+    }
+
+    //...
+}
