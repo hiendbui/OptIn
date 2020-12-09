847750c0e3a68f006dedf385ab7e7071d8986561
hiendbui <hiendbui0@gmail.com>
2020-12-09 01:56:07 -0800

    Set up nav bar components and routes

---
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class NavBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>NavBar</h1>
+            </div>
+        )
+    }
+}
--- frontend/components/news_feed/news_feed.jsx
+++ frontend/components/news_feed/news_feed.jsx
@@ -4,7 +4,7 @@ export default class NewsFeed extends React.Component {
     render() {
         return (
             <div>
-                <h1>News Feed</h1>
+                <p>News Feed</p>
                 <button onClick={()=>this.props.logout()}>Log out</button>
             </div>
         )
