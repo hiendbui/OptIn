de51933e011edb3d895862de378f2d35ff75862f
hiendbui <hiendbui0@gmail.com>
2020-12-09 11:46:12 -0800

    Write validations and associations for profiles

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id          :bigint           not null, primary key
+#  user_id     :integer          not null
+#  full_name   :string           not null
+#  headline    :string           not null
+#  location    :string           not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
