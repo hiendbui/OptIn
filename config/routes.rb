93395eecfb30c19aad9834e45f279fa262d17e67
hiendbui <hiendbui0@gmail.com>
2020-12-14 00:52:11 -0800

    Write routes for profile items

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :destroy]
     resource :session, only: [:create, :destroy]
-    resources :profiles, only: [:index, :create, :update, :show]
+    resources :profiles, only: [:index, :create, :update, :show] do 
+      resources :experiences, only: [:index, :create, :update, :destroy]
+      resources :educations, only: [:index, :create, :update, :destroy]
+      resources :achievements, only: [:index, :create, :update, :destroy]
+    end
   end
 
   root 'static_pages#root'
