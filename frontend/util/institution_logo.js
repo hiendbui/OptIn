829d752869b7f69de67837c279ad3ab56aba1587
hiendbui <hiendbui0@gmail.com>
2020-12-15 00:23:41 -0800

    Add logo data

---
+++ frontend/util/institution_logo.js
@@ -0,0 +1,10 @@
+
+const fetchLogo = (institution) => ( 
+    $.ajax({ 
+        method: 'GET', 
+        url: `https://autocomplete.clearbit.com/v1/companies/suggest?query=${institution.split(' ').join('').toLowerCase()}`,
+    })
+);
+
+
+export default fetchLogo
