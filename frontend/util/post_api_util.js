19a283eabf0e53cc782b31556310d4f354c055c1
hiendbui <hiendbui0@gmail.com>
2020-12-17 19:01:49 -0800

    Finish jquery functions for posts

---
+++ frontend/util/post_api_util.js
@@ -0,0 +1,33 @@
+export const createPost = (post) => (
+    $.ajax({
+        method: 'POST',
+        url: '/api/posts',
+        data:  post,
+        contentType: false,
+        processData: false 
+    })
+)
+
+//remember to append post id to post data
+export const updatePost = (post) => {
+    return $.ajax({
+        method: 'PATCH',
+        url: `/api/posts/${post.id}`,
+        data: post,
+        contentType: false,
+        processData: false
+    })
+}
+
+export const fetchAllPosts = () => (
+    $.ajax({
+        url: '/api/posts',
+    })
+)
+
+export const destroyPost = (postId) => (
+    $.ajax({
+        method: 'DELETE',
+        url: `/api/posts/${postId}`,
+    })
+)
