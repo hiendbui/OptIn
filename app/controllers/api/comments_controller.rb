4c53f500c6ac7d089c08ddcc9737d74cc60b1d2f
hiendbui <hiendbui0@gmail.com>
2020-12-17 18:31:46 -0800

    Finish controllers for posts and comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,39 @@
 class Api::CommentsController < ApplicationController
+    def index
+        @comments = Comment.all
+        render 'api/comments/index'
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.author_id = current_user.profile.id
+
+        if @comment.save
+            render 'api/comments/show'
+        else
+            render json: @comment.errors.full_messages, status: 422
+        end
+    end
+
+    
+    
+    def update
+        @comment = Comment.find(params[:id])
+        if @comment.update(comment_params)
+            render "api/comments/show"
+        else
+            render json: @comment.errors.full_messages, status: 422
+        end
+    end
+    
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.delete if (@comment) 
+    end
+    
+    private
+    
+    def comment_params
+        params.require(:comment).permit(:body, :post_id)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
     validates :body, :author_id, presence: true 
 
+    has_one_attached :photo
+
     belongs_to :profile,
         primary_key: :id,
         foreign_key: :author_id,
