5d8599e5a4cbe68047fb633cfe3cfd47c8a39448
hiendbui <hiendbui0@gmail.com>
2020-12-17 20:07:35 -0800

    Finish post actions/action creators

--- app/views/api/posts/index.json.jbuilder
+++ app/views/api/posts/index.json.jbuilder
@@ -1,12 +1,4 @@
 @posts.each do |post|
   json.set! post.id do
     json.partial! "api/posts/post", post: post
-    json.comments do
-    post.comments.each do |comment|
-        json.set! comment.id do
-        json.extract! comment, :id, :body, :author_id, :post_id, :created_at, :updated_at
-        end
-    end
-end
-  end
 end
--- frontend/actions/news_feed_actions/post_actions.js
+++ frontend/actions/news_feed_actions/post_actions.js
@@ -4,4 +4,39 @@ export const RECEIVE_POST = 'RECEIVE_POST';
 export const REMOVE_POST = 'REMOVE_POST';
 export const RECEIVE_ALL_POSTS = 'RECEIVE_ALL_POSTS';
 
-export
+const receivePost = post => ({
+    type: RECEIVE_CONNECTION,
+    post
+})
+
+const removePost = postId => ({
+    type: REMOVE_CONNECTION,
+    postId
+})
+
+const receiveAllPosts = (posts) => ({
+    type: RECEIVE_ALL_POSTS,
+    posts
+})
+
+
+export const createPost = post => dispatch => (
+    APIUtil.createPost(post)
+        .then(post => dispatch(receivePost(post)))
+)
+
+export const updatePost = post => dispatch => (
+    APIUtil.updatePost(post)
+        .then(post => dispatch(receivePost(post)))
+) //remember to append post id to post data
+
+
+export const fetchAllPosts = () => dispatch => (
+    APIUtil.fetchAllPosts()
+        .then(posts => dispatch(receiveAllPosts(posts)))
+)
+
+export const destroyPost = postId => dispatch => (
+    APIUtil.destroyPost(postId)
+        .then(() => dispatch(removePost(postId)))
+)
