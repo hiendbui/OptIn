903572ec310722543e77037cab53ba9fd29d03f4
hiendbui <hiendbui0@gmail.com>
2020-12-17 19:32:50 -0800

    Change jbuilder for posts

---
+++ frontend/actions/news_feed_actions/post_actions.js
@@ -0,0 +1,7 @@
+import * as APIUtil from '../../util/post_api_util'
+
+export const RECEIVE_POST = 'RECEIVE_POST';
+export const REMOVE_POST = 'REMOVE_POST';
+export const RECEIVE_ALL_POSTS = 'RECEIVE_ALL_POSTS';
+
+export
