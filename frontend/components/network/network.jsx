322f052e45b6f39ab0229b069cafa92a71109e6a
hiendbui <hiendbui0@gmail.com>
2021-01-20 14:01:52 -0800

    Refactor network code

--- frontend/components/network/network.jsx
+++ frontend/components/network/network.jsx
@@ -27,6 +27,10 @@ export default class Network extends React.Component {
         }
     }
 
+    profilePath(profile) {
+        return `/in/${profile.fullName.toLowerCase().split(' ').join('-')}-${profile.id}`
+    }
+
     render() {
         return (
             <div className='network'>
@@ -40,7 +44,7 @@ export default class Network extends React.Component {
                         return(
                             <div className='profile-block' key={profile.id}>
                                 <img className='cover' src="https://static-exp1.licdn.com/sc/h/cpemy7gsm8bzfb5nnbbnswfdm" alt="" />
-                                <div className='link'><Link to={{ pathname: `/in/${profile.fullName.toLowerCase().split(' ').join('-')}-${profile.id}`, state:{profile: {profile} }}}><div> <img className='profile-img' src={
+                                <div className='link'><Link to={{ pathname: this.profilePath(profile), state:{profile: {profile} }}}><div> <img className='profile-img' src={
                                     profile.photoUrl ?
                                         profile.photoUrl :
                                         'https://optin-dev.s3-us-west-1.amazonaws.com/default_profile.png'}
@@ -64,7 +68,7 @@ export default class Network extends React.Component {
                             return (
                                 <div className='profile-block' key={profile.id}>
                                     <img className='cover' src="https://static-exp1.licdn.com/sc/h/cpemy7gsm8bzfb5nnbbnswfdm" alt="" />
-                                    <div className='link'><Link to={{ pathname: `/in/${profile.fullName.toLowerCase().split(' ').join('-')}-${profile.id}`, state: { profile: profile } }}><div> <img className='profile-img' src={
+                                    <div className='link'><Link to={{ pathname: this.profilePath(profile), state: { profile: profile } }}><div> <img className='profile-img' src={
                                         profile.photoUrl ?
                                             profile.photoUrl :
                                             'https://optin-dev.s3-us-west-1.amazonaws.com/default_profile.png'}
