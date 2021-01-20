9f4886db162caafed4cbcb6697ef83e481e43946
hiendbui <hiendbui0@gmail.com>
2021-01-19 18:09:33 -0800

    Clean up news feed code

--- frontend/components/news_feed/news_feed.jsx
+++ frontend/components/news_feed/news_feed.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en';
 import ReactTimeAgo from 'react-time-ago';
-import { BsPencilSquare } from 'react-icons/bs';
+import { BsPencilSquare, BsThreeDots } from 'react-icons/bs';
 import { IconContext } from "react-icons"
 import  SideBarContainer from '../sidebar/sidebar_container'
 
@@ -64,18 +64,24 @@ export default class NewsFeed extends React.Component {
             <div className='news-feed'>
                 <div className='user-profile'>
                     <img className='cover' src="https://static-exp1.licdn.com/sc/h/cpemy7gsm8bzfb5nnbbnswfdm" alt="" />
-                    <div className='link'><Link to={{ pathname: `/in/${this.profile?.fullName.toLowerCase().split(' ').join('-')}-${this.profile?.id}` } }><div> <img className='profile-img' src={
-                        this.profile?.photoUrl ?
+                    <div className='link'>
+                        <Link to={{ pathname: `/in/${this.profile?.fullName.toLowerCase().split(' ').join('-')}-${this.profile?.id}` } }>
+                    <div> 
+                    <img className='profile-img' 
+                        src={
+                            this.profile?.photoUrl ?
                             this.profile.photoUrl :
-                            'https://optin-dev.s3-us-west-1.amazonaws.com/default_profile.png'}
+                            'https://optin-dev.s3-us-west-1.amazonaws.com/default_profile.png'
+                        }
                         width="75"
-                        height="75" /> <p className='name'>{this.profile?.fullName}</p></div></Link>
-
+                        height="75" /> 
+                    <p className='name'>{this.profile?.fullName}</p></div></Link>
                     <p className='headline'>{this.profile?.headline}</p>
                     <br/>
                         
                         <Link  to='/mynetwork'>
-                            <p className='connection'>Connections<span className='num'>{this.props.connections ? this.props.connections : ''}</span></p>
+                            <p className='connection'>Connections
+                            <span className='num'>{this.props.connections ? this.props.connections : ''}</span></p>
                             <p className='line'>Manage your network</p>
                         </Link>
                         
@@ -84,23 +90,49 @@ export default class NewsFeed extends React.Component {
                 <div className='post-block'>
                     <div className='create-post'>
                         <form onSubmit={this.handleSubmit}>
-                            <div className="body"><IconContext.Provider value={{ style: { fontSize: '20px' } }}><BsPencilSquare /></IconContext.Provider><textarea placeholder="Start a Post" required="required" id="input" cols="30" width="100%" onChange={this.handleChange}></textarea></div>
-                            <span className='img-input'>Upload Photo:</span><input type="file" onChange={this.handleFile} />
+                            <div className="body">
+                                <IconContext.Provider value={{ style: { fontSize: '20px' } }}>
+                                    <BsPencilSquare />
+                                </IconContext.Provider>
+                                <textarea 
+                                        placeholder="Start a Post" 
+                                        required="required" 
+                                        id="input" 
+                                        cols="30" 
+                                        width="100%" 
+                                        onChange={this.handleChange}>
+                                </textarea>
+                            </div>
+                            <span className='img-input'>Upload Photo:</span>
+                            <input type="file" onChange={this.handleFile} />
                             <button>Post</button>
                         </form>
                     </div>
                     <div id='line'></div>
                     {[...this.props.postsArr].reverse().map((post) => {
-                        let profile = this.props.profiles[post.authorId]
+                        let profile = this.props.profiles[post.authorId];
+                        let profilePath = profile?.fullName.toLowerCase().split(' ').join('-');
                         if (profile)
                         return <div className="post" key={post.id}>
-                            <Link to={{ pathname: `/in/${profile.fullName.toLowerCase().split(' ').join('-')}-${profile.id}` } }>
+                            <IconContext.Provider value={{ style: { fontSize: '20px', float:'right' } }}>
+                                <BsThreeDots />
+                            </IconContext.Provider>
+                            <Link to={{ pathname: `/in/${profilePath}-${profile.id}` } }>
                             <div className='prof'>
-                                <img src={profile.photoUrl ? profile.photoUrl : 'https://optin-dev.s3-us-west-1.amazonaws.com/default_profile.png'} alt=""/>
+                                <img src={profile.photoUrl ? 
+                                            profile.photoUrl : 
+                                            'https://optin-dev.s3-us-west-1.amazonaws.com/default_profile.png'} 
+                                />
                                 <div className='dets'>
                                 <p >{profile.fullName}</p>
                                 <p>{profile.headline}</p>
-                                        <p><ReactTimeAgo fontSize="12px" date={new Date(post.createdAt)} locale="en" timeStyle="mini-minute-now" /></p>
+                                        <p><ReactTimeAgo 
+                                                fontSize="12px" 
+                                                date={new Date(post.createdAt)} 
+                                                locale="en" 
+                                                timeStyle="mini-minute-now" 
+                                            />
+                                        </p>
                                 </div>
                             </div>
                             </Link>
@@ -110,10 +142,11 @@ export default class NewsFeed extends React.Component {
                             <br/>
                             <div className="comments">
                                 {this.props.comments.map((comment => {
-                                    let profile = this.props.profiles[comment.authorId]
+                                    let profile = this.props.profiles[comment.authorId];
+                                    let profilePath = profile?.fullName.toLowerCase().split(' ').join('-');
                                     if (comment.postId == post.id)
                                         return <div className='item'key={comment.id}>
-                                                <Link to={{ pathname: `/in/${profile.fullName.toLowerCase().split(' ').join('-')}-${profile.id}` } }>
+                                                <Link to={{ pathname: `/in/${profilePath}-${profile.id}` } }>
                                                     <img className='img-comment' src={
                                                     profile.photoUrl ?
                                                     profile.photoUrl :
@@ -121,7 +154,15 @@ export default class NewsFeed extends React.Component {
                                                     />
                                                 </Link> 
                                                  <div className="body">
-                                                <p className="full">{profile.fullName}</p><p className="time"><ReactTimeAgo fontSize="12px" date={new Date(comment.createdAt)} locale="en" timeStyle="mini-minute-now" /></p>
+                                                <p className="full">{profile.fullName}</p>
+                                                <p className="time">
+                                                    <ReactTimeAgo 
+                                                        fontSize="12px" 
+                                                        date={new Date(comment.createdAt)} 
+                                                        locale="en" 
+                                                        timeStyle="mini-minute-now" 
+                                                    />
+                                                </p>
                                                 <p className="head">{profile.headline}</p>
                                                 <p className="bod">{comment.body}</p>
                                                 </div>
@@ -134,7 +175,12 @@ export default class NewsFeed extends React.Component {
                                     'https://optin-dev.s3-us-west-1.amazonaws.com/default_profile.png'}
                                  />
                             <form onSubmit={this.handleComment(post.id)}>
-                            <input className='comment' placeholder="Add a comment..." type="text" onChange={this.handleComChange}/>
+                            <input 
+                                className='comment' 
+                                placeholder="Add a comment..." 
+                                type="text" 
+                                onChange={this.handleComChange}
+                            />
                             <button>Post</button>
                             </form>
                             </div>
