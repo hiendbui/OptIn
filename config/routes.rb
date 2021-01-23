da813d017eba19be1f967d6c5c80e1abc0247734
hiendbui <hiendbui0@gmail.com>
2021-01-23 14:10:12 -0800

    Add :index to experience and education routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :destroy]
     resource :session, only: [:create, :destroy]
     resources :profiles, only: [:index, :create, :update, :show]
-    resources :experiences, only: [ :create, :update, :destroy]
-    resources :educations, only: [ :create, :update, :destroy]
+    resources :experiences, only: [:index, :create, :update, :destroy]
+    resources :educations, only: [:index, :create, :update, :destroy]
     resources :achievements, only: [ :create, :update, :destroy]
     resources :connections, only: [:create, :destroy, :show]
     resources :posts, only: [:create, :index, :update, :destroy] do 
